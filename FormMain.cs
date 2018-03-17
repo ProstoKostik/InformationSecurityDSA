1d81a2403729457adb1ba7533bb704acec61ade6
443_Todorenko <Kostya.todorenko@mail.ru>
2018-03-17 13:02:55 +0300

    Добавьте файлы проекта.

---
+++ FormMain.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Numerics;
+
+namespace DSA
+{
+    public partial class FormMain : Form
+    {
+        /// <summary>
+        /// Параметр q
+        /// </summary>
+        private BigInteger _paramQ;
+        /// <summary>
+        /// Параметр p
+        /// </summary>
+        private BigInteger _paramP;
+        /// <summary>
+        /// Результат частного (p-1)/q
+        /// </summary>
+        private BigInteger _paramT;
+        /// <summary>
+        /// Заданный пользователем параметр q
+        /// </summary>
+        private BigInteger _tmpQ;
+        /// <summary>
+        /// Заданный пользователем параметр p
+        /// </summary>
+        private BigInteger _tmpP;
+        /// <summary>
+        /// Цифровая подпись
+        /// </summary>
+        private DigitalSignatureAlgorithm _DSA;
+        /// <summary>
+        /// Инициализация компонентов формы
+        /// </summary>
+        public FormMain()
+        {
+            InitializeComponent();
+            _tmpP = BigInteger.Zero;
+            _tmpQ = BigInteger.Zero;
+            _paramQ = new BigInteger(142433);
+            _paramT = new BigInteger(6);
+            _paramP = _paramQ * _paramT + BigInteger.One;        
+        }
+        /// <summary>
+        /// Выбор пользовательских настроек
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UserInput_CheckedChanged(object sender, EventArgs e)
+        {
+            if (userInput.Checked)
+            {
+                userParams.Enabled = true;
+            }
+            else
+            {
+                userParams.Enabled = false;
+            }
+        }
+        /// <summary>
+        /// Изменение текста
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Text_TextChanged(object sender, EventArgs e)
+        {
+            if (text.Text.Length != 0)
+            {
+                Generate.Enabled = true;
+                if (gen.Text.Length != 0)
+                {
+                    Check.Enabled = true;
+                }
+            }
+            else
+            {
+                Generate.Enabled = false;
+                Check.Enabled = false;
+            }
+        }
+        /// <summary>
+        /// Создание электронной подписи
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Generate_Click(object sender, EventArgs e)
+        {
+            if (_paramP <= _paramQ)
+            {
+                message.Text = "Неверно заданы параметры";
+                message.ForeColor = System.Drawing.Color.DeepPink;
+                message.Visible = true;
+            }
+            else
+            {
+                P.Text = _paramP.ToString();
+                Q.Text = _paramQ.ToString();
+                _DSA = new DigitalSignatureAlgorithm(_paramQ, _paramP);
+                StringBuilder SB = _DSA.GenerateDigitalSignature(text.Text);
+                gen.Clear();
+                gen.AppendText(SB.ToString());
+                Check.Enabled = true;
+            }
+        }
+        /// <summary>
+        /// Изменение параметра q
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Q_TextChanged(object sender, EventArgs e)
+        {
+            message.Visible = false;
+            if (Q.Text.Length != 0)
+            {
+                if (!BigInteger.TryParse(Q.Text, out _tmpQ))
+                {
+                    wrongQ.Visible = true;
+                }
+                else
+                {
+                    _paramQ = _tmpQ;
+                    wrongQ.Visible = false;
+                }
+
+            }
+            else
+            {
+                _paramQ = int.MaxValue;
+                wrongQ.Visible = false;
+            }
+        }
+        /// <summary>
+        /// Изменение параметра p
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void P_TextChanged(object sender, EventArgs e)
+        {
+            message.Visible = false;
+            if (P.Text.Length != 0)
+            {
+                if (!BigInteger.TryParse(P.Text, out _tmpP))
+                {
+                    wrongP.Visible = true;
+                }
+                else
+                {
+                    _paramP = _tmpP;
+                    wrongP.Visible = false;
+                }
+
+            }
+            else
+            {
+                _paramP = _paramQ * _paramT + BigInteger.One;
+                wrongP.Visible = false;
+            }
+        }
+        /// <summary>
+        /// Проверка электронной подписи
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Check_Click(object sender, EventArgs e)
+        {
+            StringBuilder SB = _DSA.CheckDigitalSignature(text.Text, gen.Lines, ref message);
+            ch.Clear();
+            ch.AppendText(SB.ToString());
+        }
+    }
+}
---
+++ Sign.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Numerics;
+using System.Windows.Forms;
+
+namespace DSA
+{
+    /// <summary>
+    /// Класс, реализующий DSA
+    /// </summary>
+    public class DigitalSignatureAlgorithm
+    {
+        /// <summary>
+        /// Параметр q
+        /// </summary>
+        private BigInteger _paramQ;
+        /// <summary>
+        /// Параметр p
+        /// </summary>
+        private BigInteger _paramP;
+        /// <summary>
+        /// Результат частного (p-1)/q
+        /// </summary>
+        private BigInteger _paramT;
+        /// <summary>
+        /// Параметр g
+        /// </summary>
+        private BigInteger _paramG;
+        /// <summary>
+        /// Закрытый ключ
+        /// </summary>
+        private BigInteger _paramX;
+        /// <summary>
+        /// Параметр y
+        /// </summary>
+        private BigInteger _paramY;
+
+        /// <summary>
+        /// DSA с параметрами q и p
+        /// </summary>
+        /// <param name="q">q</param>
+        /// <param name="p">p</param>
+        public DigitalSignatureAlgorithm(BigInteger q, BigInteger p)
+        {
+            _paramQ = q;
+            _paramP = p;
+            _paramT = (p - BigInteger.One) / q;
+            SetParamG();
+            _paramX = GetRandom(_paramQ);
+            _paramY = BigInteger.ModPow(_paramG, _paramX, _paramP);
+        }
+
+        /// <summary>
+        /// Устанавливает рандомное значение заданного параметра, которое больше 0 и не превышает maxValue
+        /// </summary>
+        /// <param name="maxValue">Верхняя граница параметра</param>
+        private BigInteger GetRandom(BigInteger maxValue)
+        {
+            Random rand = new Random();
+            double tmp1 = rand.NextDouble();
+            double tmp2 = rand.NextDouble();
+            int tmp3 = rand.Next();
+
+            BigInteger b1 = new BigInteger(UInt64.MaxValue * tmp1);
+            BigInteger b2 = new BigInteger(UInt64.MaxValue * tmp2);
+            BigInteger b3 = BigInteger.ModPow(10, tmp3, _paramQ);
+            BigInteger result = (b1 * b3 + b2) % (_paramQ - 1);
+            if (result == BigInteger.Zero) {
+                return BigInteger.One;
+            }
+            return result;
+        }
+        /// <summary>
+        /// Задать число g, такого, что  его мультипликативный порядок по модулю p равен q
+        /// </summary>
+        private void SetParamG()
+        {
+            BigInteger h = BigInteger.One;
+            do
+            {
+                h += BigInteger.One;
+                _paramG = BigInteger.ModPow(h, _paramT, _paramP);
+            }
+            while (_paramG < 2);
+        }
+        /// <summary>
+        /// Подпись сообщения
+        /// </summary>
+        /// <param name="text">Текст для подписи</param>
+        /// <returns>Подпись, пара r и s</returns>
+        public StringBuilder GenerateDigitalSignature(string text)
+        {
+
+            BigInteger H = GetHashCode(text);
+            BigInteger k;
+            BigInteger r;
+            BigInteger s;
+            do
+            {
+                do
+                {
+                    //Выбор случайного числа, от 0 до q
+                    k = GetRandom(_paramQ);
+                    //Вычисление параметра r
+                    r = BigInteger.ModPow(_paramG, k, _paramP) % _paramQ;
+                }
+                while (r == BigInteger.Zero);
+                //Вычисление параметра s
+                s = (BigInteger.ModPow(k, _paramQ - 2, _paramQ) * ((H + _paramX * r) % _paramQ)) % _paramQ;
+            }
+            while (s == BigInteger.Zero);
+
+            StringBuilder SB = new StringBuilder();
+            SB.AppendLine("r = " + r.ToString());
+            SB.AppendLine("s = " + s.ToString());
+            //Подпись
+            return SB;
+        }
+        /// <summary>
+        /// Проверка подписи
+        /// </summary>
+        /// <param name="text">Текст для проверки </param>
+        /// <param name="key">Ключ</param>
+        /// <param name="message">Сообщение</param>
+        /// <returns>Результат проверки по внешнему ключу</returns>
+        public StringBuilder CheckDigitalSignature(string text, string[] key, ref Label message)
+        {
+            BigInteger r = BigInteger.Parse(key[0].Substring(4));
+            BigInteger s = BigInteger.Parse(key[1].Substring(4));
+            BigInteger H = GetHashCode(text);
+           
+            //Вычисление параметра w
+            BigInteger w = BigInteger.ModPow(s, _paramQ - 2, _paramQ);
+            //Вычисление параметра u1
+            BigInteger u1 = (H * w) % _paramQ;
+            //Вычисление параметра u2
+            BigInteger u2 = (r * w) % _paramQ;
+            //Вычисление параметра v
+            BigInteger v = GetParamV(u1, u2);
+            //Если параметр v = r, то подпись верна
+            if (v == r)
+            {
+                message.Text = "Текст корректен";
+                message.ForeColor = System.Drawing.Color.Blue;
+                message.Visible = true;
+            }
+            else
+            {
+                message.Text = "Текст был изменен или неверно заданы параметры";
+                message.ForeColor = System.Drawing.Color.Red;
+                message.Visible = true;
+            }
+            return new StringBuilder("v = " + v.ToString());
+        }
+        /// <summary>
+        /// Получить параметр v
+        /// </summary>
+        /// <param name="u1">Параметр u1</param>
+        /// <param name="u2">Параметр u2</param>
+        /// <returns>Параметр v</returns>
+        private BigInteger GetParamV(BigInteger u1, BigInteger u2)
+        {
+            BigInteger v1 = BigInteger.ModPow(_paramG, u1, _paramP);
+            BigInteger v2 = BigInteger.ModPow(_paramY, u2, _paramP);
+            BigInteger v3 = (v1 * v2) % _paramP;
+
+            return (v1 * v2) % _paramP;
+        }
+
+        /// <summary>
+        /// Получить хеш-код сообщения
+        /// </summary>
+        /// <param name="text">Текст</param>
+        /// <returns>Хеш-код</returns>
+        private BigInteger GetHashCode(string text)
+        {
+            SHA256 hash = new SHA256Managed();
+            byte[] hashBYTE = hash.ComputeHash(Encoding.Default.GetBytes(text));
+            BigInteger hashINT = new BigInteger(hashBYTE);
+
+            BigInteger result = hashINT & _paramQ;
+            return result;
+        }
+
+    }
+}
+ 
